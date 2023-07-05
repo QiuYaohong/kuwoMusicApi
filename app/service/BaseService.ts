8b61ede6d1e061a65f5080cfe6006b3162f51f25
Gamom <63381531+wifi-left@users.noreply.github.com>
2023-07-05 10:22:25 +0800

    Update BaseService.ts
    精简Cookie内容，修复无法搜索

--- app/service/BaseService.ts
+++ app/service/BaseService.ts
@@ -4,17 +4,17 @@ import { v4 as uuidv4 } from 'uuid'
 class BaseService extends Service {
   _headers (opts) {
     return {
-      Cookie: 'Hm_lvt_cdb524f42f0ce19b169a8071123a4797=1623339177,1623339183; _ga=GA1.2.1195980605.1579367081; Hm_lpvt_cdb524f42f0ce19b169a8071123a4797=1623339982; kw_token=3E7JFQ7MRPL; _gid=GA1.2.747985028.1623339179; _gat=1',
+      Cookie: 'kw_token=3E7JFQ7MRPL; Hm_token=a5k3Rn3zi52eF3zEyQ3EQjb2HfbaCZDr',
       csrf: '3E7JFQ7MRPL',
       Host: 'www.kuwo.cn',
       Referer: 'http://www.kuwo.cn/',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+      Cross: '4ef7fff9eeb7a38b89bf3fbb5a6a111e',
       ...opts,
     }
   }
-
+  
   timeoutCount = 0
-
   async commonRequest (url, options) {
     const opts = {
       method: 'GET',
