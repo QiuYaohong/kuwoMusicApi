7d8d089984d727ef9ce4e9a896b2d51794385d68
qyhqiu <843240938@qq.com>
2023-07-19 22:29:16 +0800

    fix: secret

--- app/service/BaseService.ts
+++ app/service/BaseService.ts
@@ -1,11 +1,13 @@
 import { Service } from 'egg'
 import { v4 as uuidv4 } from 'uuid'
 
+import { h, v, f, Cookie } from '../utils/secret'
+
 class BaseService extends Service {
   _headers (opts) {
     return {
-      Cookie: 'Hm_lvt_cdb524f42f0ce19b169a8071123a4797=1623339177,1623339183; _ga=GA1.2.1195980605.1579367081; Hm_lpvt_cdb524f42f0ce19b169a8071123a4797=1623339982; kw_token=3E7JFQ7MRPL; _gid=GA1.2.747985028.1623339179; _gat=1',
-      csrf: '3E7JFQ7MRPL',
+      Cookie,
+      Secret: h(Object(v)(f), f),
       Host: 'www.kuwo.cn',
       Referer: 'http://www.kuwo.cn/',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
--- app/service/playUrl.ts
+++ app/service/playUrl.ts
@@ -1,7 +1,7 @@
 const BaseService = require('./BaseService')
 
 export default class PlayUrl extends BaseService {
-  async getPlayUrl (mid, type = 'convert_url3', br = '128kmp3') {
-    return this.commonRequest(`http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${mid}&type=${type}&httpsStatus=1&br=${br}`)
+  async getPlayUrl (mid, type = 'music', br = '128kmp3') {
+    return this.commonRequest(`http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${mid}&type=${type}&httpsStatus=1&plat=web_www&from=&br=${br}`)
   }
 }
