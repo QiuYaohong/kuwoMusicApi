948e088a2e25a5dfd0c0c2fe8a65469eb99f687f
qyhqiu <843240938@qq.com>
2023-07-22 13:56:58 +0800

    feat:  corn clear logs

--- app/schedule/logs.ts
+++ app/schedule/logs.ts
@@ -1,24 +1,19 @@
-const path = require('path')
 const fse = require('fs-extra')
-const appDirectory = fse.realpathSync(process.cwd())
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
 //  https://www.eggjs.org/zh-CN/basics/schedule#%E5%AE%9A%E6%97%B6%E6%96%B9%E5%BC%8F
 module.exports = app => {
-  console.log('app-->', app.config)
   return {
     schedule: {
-      //  每 24 h
+      //  每 24 h 清除 logs
       interval: '86440s',
       type: 'all', // 指定所有的 worker 都需要执行
     },
-    async task (ctx) {
-      fse.emptydirSync(resolveApp('logs'), err => {
+    async task () {
+      fse.emptydirSync(app.config?.logger?.dir, err => {
         if (!err) {
           return console.log('emptydirSync success')
         }
       })
-      ctx.app.cache = 1
     },
   }
 }
